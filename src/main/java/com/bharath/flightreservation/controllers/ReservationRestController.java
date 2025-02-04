202ab2e641d7cdf3ffc839889b479a62eaa48010
Felix Nunez <felix.nunez@vass.com.mx>
2025-02-04 10:24:54 -0700

    CorssOrigin fixed

--- src/main/java/com/bharath/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/com/bharath/flightreservation/controllers/ReservationRestController.java
@@ -1,9 +1,11 @@
 package com.bharath.flightreservation.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bharath.flightreservation.dtos.ReservationUpdateRequest;
@@ -11,6 +13,8 @@ import com.bharath.flightreservation.entities.Reservation;
 import com.bharath.flightreservation.services.ReservationService;
 
 @RestController
+@RequestMapping("/reservations")
+@CrossOrigin(origins = "http://localhost:4200") // Allow this origin
 public class ReservationRestController {
 
 	private final ReservationService reservationService;
@@ -19,13 +23,13 @@ public class ReservationRestController {
         this.reservationService = reservationService;
     }
 
-	@GetMapping("/reservations/{id}")
+	@GetMapping("/{id}")
 	public Reservation findReservation(@PathVariable("id") Long id) {
 		return reservationService.getReservationById(id);
 
 	}
 
-	@PostMapping("/reservations")
+	@PostMapping
 	public Reservation updateReservation(@RequestBody ReservationUpdateRequest request) {
 		Reservation reservation = reservationService.getReservationById(request.getId());
 		reservation.setNumberOfBags(request.getNumberOfBags());
